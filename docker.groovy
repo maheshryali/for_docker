78fd88ef6684f3002e62450debf16d32a1e139d3
Ubuntu <ubuntu@ip-172-31-20-9.ec2.internal>
2022-12-02 16:25:39 +0000

    addedchanges

--- docker.groovy
+++ docker.groovy
@@ -6,7 +6,6 @@ pipeline {
                 sh """
                 sudo apt update
                 docker info
-                cd for_docker
                 docker image build -t nginx:latest .
                 docker image ls
                 docker run container --name nginxcontainer nginx:latest
