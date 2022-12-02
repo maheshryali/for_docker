c444613a197394328824ed4490b6e932ba224d95
Ubuntu <ubuntu@ip-172-31-20-9.ec2.internal>
2022-12-02 03:40:06 +0000

    addedchanges2

--- docker.groovy
+++ docker.groovy
@@ -6,7 +6,10 @@ pipeline {
                 sh """
                 sudo apt update
                 docker info
-                docker container run alpine 
+                docker container run  alpine 
+                docker container ls -a
+                docker container image ls
+                docker container run -it hello-world /bin/bash
                 """
             }
         }
